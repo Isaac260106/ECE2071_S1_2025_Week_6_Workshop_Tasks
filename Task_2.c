2915d9004fb67d3f2a5bbccfbf3bfd42c45c7311
Isaac <icho0016@student.monash.edu>
2025-04-07 13:36:05 +0800

    DONE

--- Task_2.c
+++ Task_2.c
@@ -97,7 +97,13 @@ void inOrder(struct node* treePtr)
 
 void delete_tree(struct node** treePtr)
 {
-       free(*treePtr);
-	   delete_tree(&((*treePtr)->leftPtr));
-       delete_tree(&((*treePtr)->rightPtr));
+	if ((*treePtr)!= NULL)
+	{
+		delete_tree(&((*treePtr)->leftPtr));
+		delete_tree(&((*treePtr)->rightPtr));	
+		free(*treePtr);
+		(*treePtr) = NULL;	
+			
+	}
+	
 }
